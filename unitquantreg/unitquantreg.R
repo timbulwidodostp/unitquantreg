172f106b0fd98a8d70e95b6242b2a728d2815d3f
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-06-13 09:23:10 +0700

    unitquantreg With (In) R Software

---
+++ unitquantreg/unitquantreg.R
@@ -0,0 +1,16 @@
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Parametric unit quantile regression models Use unitquantreg With (In) R Software
+install.packages("remotes")
+remotes::install_github("brechtdv/rineq")
+library("rineq")
+unitquantreg = read.csv("https://raw.githubusercontent.com/timbulwidodostp/unitquantreg/main/unitquantreg/unitquantreg.csv",sep = ";")
+# Estimation Parametric unit quantile regression models Use unitquantreg With (In) R Software
+unitquantreg <- unitquantreg(formula = y1 ~ x + z + I(x^2) | z + x, data = unitquantreg, family = "uweibull", tau = 0.5, link.theta = "log")
+summary(unitquantreg)
+# Parametric unit quantile regression models Use unitquantreg With (In) R Software
+# Olah Data Semarang
+# WhatsApp : +6285227746673
+# IG : @olahdatasemarang_
+# Finished
